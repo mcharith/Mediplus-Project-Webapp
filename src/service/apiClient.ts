0b7810467e332108fe9a0d6bb9b61f26e39b7033
Charith Mihiranga <156079886+mcharith@users.noreply.github.com>
2026-03-25 15:09:50 +0530

    Update API gateway URL and adjust base path in API client configuration

--- src/service/apiClient.ts
+++ src/service/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.VITE_API_GATEWAY_URL;
+export const BASE_URL = `${import.meta.env.VITE_API_GATEWAY_URL}/api/v1`;
 
 const apiClient = axios.create({
     baseURL: BASE_URL,
